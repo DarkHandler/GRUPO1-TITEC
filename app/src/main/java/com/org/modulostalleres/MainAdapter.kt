0fd52157fa3a1d1616637c1a15a4c9732e4d3f1e
DarkHandler <68122176+DarkHandler@users.noreply.github.com>
2021-12-07 20:55:37 -0300

    mostrado de talleres listo

--- app/src/main/java/com/org/modulostalleres/MainAdapter.kt
+++ app/src/main/java/com/org/modulostalleres/MainAdapter.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
-    private val activityNames = listOf<String>("Basket","Futbol","Natacion","Bodybuilding")
+    //private val activityNames = listOf<String>("Basket","Futbol","Natacion","Bodybuilding")
 
     //numOfItems
     override fun getItemCount(): Int {
@@ -24,7 +24,11 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         //val videoName = activityNames[position]
         val taller = homeFeed.talleres[position]
+        holder.view.findViewById<TextView>(R.id.button1).id = taller.codigo_actividad
         holder.view.findViewById<TextView>(R.id.textViewActivityName).text = taller.nombre_actividad
+        holder.view.findViewById<TextView>(R.id.textViewModality).text = taller.modalidad
+        holder.view.findViewById<TextView>(R.id.textViewArea).text = taller.area
+        holder.view.findViewById<TextView>(R.id.textViewFechaInicio).text = taller.fecha_inicio
     }
 
 }
--- app/src/main/java/com/org/modulostalleres/VistaTalleres.kt
+++ app/src/main/java/com/org/modulostalleres/VistaTalleres.kt
@@ -4,16 +4,12 @@ package com.org.modulostalleres
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RelativeLayout
 import android.widget.TextView
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.json.JSONArray
 import java.io.IOException
-import android.annotation.SuppressLint
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
@@ -32,135 +28,11 @@ class VistaTalleres : AppCompatActivity() {
 
         val recyclerViewMain = findViewById<RecyclerView>(R.id.recyclerViewMain)
 
-        recyclerViewMain.setBackgroundColor(Color.BLUE)
+        recyclerViewMain.setBackgroundColor(Color.GRAY)
         recyclerViewMain.layoutManager = LinearLayoutManager(this)
         //recyclerViewMain.adapter = MainAdapter()
 
         fetchJSON("talleres", recyclerViewMain) //traer datos
-    }
-
-
-    //@SuppressLint("ResourceType")
-    fun showTalleres(data: String?){ //rederizar datos en vista
-        /*
-        val jsonData =  JSONArray(data)
-        val layout = findViewById<RelativeLayout>(R.id.talleres)
-
-
-        for(i in 0 until jsonData.length()){
-
-            val width=160 //tamanios de los botones
-            val height=90
-            val item = jsonData.getJSONObject(i)
-
-            val textNombre = generateTextView(item, "nombre_actividad", 1)
-            val textModalidad = generateTextView(item, "modalidad", 2)
-            val textArea = generateTextView(item, "area", 3)
-            val textFechaInicio = generateTextView(item, "fecha_inicio", 4)
-
-
-            val btnMasDetalle = Button(this)
-            btnMasDetalle.text = "Más detalles"
-            btnMasDetalle.textSize = 10F
-            btnMasDetalle.id = 111+(item.get("codigo_actividad") as Int)
-
-            if(i == 0){
-                //PRIMER ATT
-                val rlp2 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp2.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-                textNombre.layoutParams = rlp2
-                layout.addView(textNombre)
-
-                /*
-                //SEGUNDO ATT
-                val rlp3 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp3.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-                rlp3.addRule(RelativeLayout.BELOW,textNombre.id -1)
-                textModalidad.layoutParams = rlp3
-                layout.addView(textModalidad)
-
-
-                //TERCERO ATT
-                val rlp4 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp4.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-                rlp4.addRule(RelativeLayout.BELOW,textModalidad.id -1)
-                textArea.layoutParams = rlp4
-                layout.addView(textArea)
-
-                //CUARTO ATT
-                val rlp5 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp5.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-                rlp5.addRule(RelativeLayout.BELOW,textArea.id -1)
-                textFechaInicio.layoutParams = rlp5
-                layout.addView(textFechaInicio)
-
-                 */
-
-                //BOTON
-                val rlp1: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(
-                    width, height
-                )
-                rlp1.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                rlp1.addRule(RelativeLayout.BELOW, textFechaInicio.id -1)
-                btnMasDetalle.layoutParams = rlp1
-                layout.addView(btnMasDetalle)
-
-            }else {  //si no es el primer item, se posiciona debajo parece
-                //PRIMER ATT
-                val rlp2 = RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.MATCH_PARENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT
-                )
-                rlp2.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                rlp2.addRule(RelativeLayout.BELOW, btnMasDetalle.id -1)
-                textNombre.layoutParams = rlp2
-                layout.addView(textNombre)
-
-                /*
-                //SEGUNDO ATT
-                val rlp3 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp3.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                rlp3.addRule(RelativeLayout.BELOW, textNombre.id - 1)
-                textModalidad.layoutParams = rlp3
-                layout.addView(textModalidad)
-
-                //TERCERO ATT
-                val rlp4 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp4.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                rlp4.addRule(RelativeLayout.BELOW, textModalidad.id - 1)
-                textArea.layoutParams = rlp4
-                layout.addView(textArea)
-
-                //CUARTO ATT
-                val rlp5 = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-                rlp5.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                rlp5.addRule(RelativeLayout.BELOW, textArea.id - 1)
-                textFechaInicio.layoutParams = rlp5
-                layout.addView(textFechaInicio)
-                 */
-
-                //BOTON
-                val rlp1 = RelativeLayout.LayoutParams(
-                    width, height
-                )
-                rlp1.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                rlp1.addRule(RelativeLayout.BELOW, textNombre.id -1)
-                btnMasDetalle.layoutParams = rlp1
-                layout.addView(btnMasDetalle)
-            }
-
-        }*/
-
-    }
-
-    private fun generateTextView(item: JSONObject, attribute:String, adder:Int): TextView {
-        val textView = TextView(this) //Nombre actividad
-        textView.text = "${item.get(attribute)}"
-        textView.textSize = 20F
-        textView.setTextColor(Color.WHITE)
-        textView.id = adder+(item.get("codigo_actividad") as Int)
-
-        return textView
     }
 
 
@@ -178,13 +50,13 @@ class VistaTalleres : AppCompatActivity() {
                 val body = response.body?.string()
                 println(body)
                 val gson = GsonBuilder().create()
-                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+                val homeFeed = HomeFeed(gson.fromJson(body, Array<Taller>::class.java).toList())
+
+                //println(homeFeed)
+
                 runOnUiThread {
                     recyclerViewMain.adapter = MainAdapter(homeFeed)
                 }
-                /*runOnUiThread {
-                    showTalleres(body)
-                }*/
             }
         })
     }
@@ -193,7 +65,10 @@ class VistaTalleres : AppCompatActivity() {
 
 class HomeFeed(val talleres: List<Taller>)
 
+class Taller(val codigo_actividad:Int, val nombre_actividad:String, val modalidad:String, val area:String, val fecha_inicio:String)
+
+/*
 class Taller(val rut_responsable:String, val tipo:String, val cupos:String, val direccion:String,
              val nombre_actividad:String, val estado_actividad:String, val descripcion:String,
              val fecha_inicio:String, val fecha_termino:String, val modalidad:String,
-             val requisitos:String, val area:String)
+             val requisitos:String, val area:String) */
